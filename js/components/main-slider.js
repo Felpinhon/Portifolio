c9cbc5349f07cb38d3380835c7a0748e845cda98
Felpinhon <lfelipe.lf990@gmmail.com>
2023-04-01 17:20:31 -0300

    Add Wiseup in slider

--- js/components/main-slider.js
+++ js/components/main-slider.js
@@ -3,12 +3,12 @@ document.getElementById("radio1").checked = true;
 
 setInterval( function(){
     nextImage();
-}, 4000)
+}, 3500)
 
 function nextImage() {
     count++;
 
-    if(count>4) {
+    if(count>5) {
         count = 1;
     }
 
